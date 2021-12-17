92f4d1ff76323dc235c43be48b19318070ab6272
dIONIs-arch <Kluev1179@gmail.com>
2021-12-18 00:07:39 +0300

    2_laba

---
+++ 2_laba/2_laba/A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_laba
+{
+    class A
+    {
+        protected double x1;
+
+        public double linear_equation(double a, double b)
+        {
+            if (a == 0)
+                throw new Exception("Нет корней");
+            else
+                return x1 = -b / a;
+        }
+    }
+}
---
+++ 2_laba/2_laba/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
